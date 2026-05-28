49fb190334a52d284cffc3c29b27fd817ea3618d
Sam Noble <i-love-to-code@users.noreply.github.com>
2026-05-28 21:09:42 +0700

    Add support for BSC network (mainnet + testnet) (#3)
    * Add support for BSC network (mainnet + testnet)
    
    * Alias BSC mainnet admin address and explain 0 initial supply on BSC
    
    * Rename variable for clarity
    
    * Stricter chain ID checks

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,17 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 
+uint256 constant BASE_MAINNET_CHAINID = 8453;
+uint256 constant BASE_TESTNET_CHAINID = 84532; // Sepolia
+uint256 constant BSC_MAINNET_CHAINID = 56;
+uint256 constant BSC_TESTNET_CHAINID = 97;
+uint256 constant LOCAL_CHAINID = 31337;
+
+address constant BASE_MAINNET_ADMIN = 0xd2b8875b840D3BD574E1e6b440888e110632A0FD;
+address constant BASE_TESTNET_ADMIN = 0xfEDB58C317d347e265990888919879a5d392a12c;
+address constant BSC_MAINNET_ADMIN = BASE_MAINNET_ADMIN;
+address constant BSC_TESTNET_ADMIN = 0x9FF0FB8e246ac58b17Acf9b7D43B76E2D2e6Bf03;
+
 contract HelperConfig is Script {
     struct NetworkConfig {
         address initialAdmin;
@@ -21,13 +32,22 @@ contract HelperConfig is Script {
 
     constructor() {
         address adminAddress;
+        uint256 initialSupply = INITIAL_SUPPLY;
 
-        if (block.chainid == 8453) {
+        if (block.chainid == BASE_MAINNET_CHAINID) {
             // Base Mainnet
-            adminAddress = 0xd2b8875b840D3BD574E1e6b440888e110632A0FD;
-        } else if (block.chainid == 84532) {
-            // Base Sepolia Testnet
-            adminAddress = 0xfEDB58C317d347e265990888919879a5d392a12c;
+            adminAddress = BASE_MAINNET_ADMIN;
+        } else if (block.chainid == BASE_TESTNET_CHAINID) {
+            // Base Testnet
+            adminAddress = BASE_TESTNET_ADMIN;
+        } else if (block.chainid == BSC_MAINNET_CHAINID) {
+            // BSC Mainnet (initial supply set to 0 as the token is bridged)
+            adminAddress = BSC_MAINNET_ADMIN;
+            initialSupply = 0 ether;
+        } else if (block.chainid == BSC_TESTNET_CHAINID) {
+            // BSC Testnet (initial supply set to 0 as the token is bridged)
+            adminAddress = BSC_TESTNET_ADMIN;
+            initialSupply = 0 ether;
         } else {
             // Local / Anvil (Default Foundry Sender) (common known address)
             adminAddress = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
@@ -37,7 +57,7 @@ contract HelperConfig is Script {
         require(adminAddress != address(0), "HelperConfig: Admin address cannot be zero");
 
         activeNetworkConfig = NetworkConfig({
-            initialAdmin: adminAddress, initialSupply: INITIAL_SUPPLY, maxSupply: MAX_SUPPLY, name: NAME, symbol: SYMBOL
+            initialAdmin: adminAddress, initialSupply: initialSupply, maxSupply: MAX_SUPPLY, name: NAME, symbol: SYMBOL
         });
     }
 }
---
+++ test/HelperConfig.t.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {HelperConfig, LOCAL_CHAINID} from "../script/HelperConfig.s.sol";
+
+contract HelperConfigTest is Test {
+    function test_BaseMainnetConfig() public {
+        vm.chainId(8453);
+        HelperConfig config = new HelperConfig();
+        (address initialAdmin, uint256 initialSupply, uint256 maxSupply, string memory name, string memory symbol) =
+            config.activeNetworkConfig();
+
+        assertEq(initialAdmin, 0xd2b8875b840D3BD574E1e6b440888e110632A0FD);
+        assertEq(initialSupply, 1_000_000_000 ether);
+        assertEq(maxSupply, 1_000_000_000 ether);
+        assertEq(name, "Veera Token");
+        assertEq(symbol, "VEERA");
+    }
+
+    function test_BaseSepoliaConfig() public {
+        vm.chainId(84532);
+        HelperConfig config = new HelperConfig();
+        (address initialAdmin, uint256 initialSupply, uint256 maxSupply, string memory name, string memory symbol) =
+            config.activeNetworkConfig();
+
+        assertEq(initialAdmin, 0xfEDB58C317d347e265990888919879a5d392a12c);
+        assertEq(initialSupply, 1_000_000_000 ether);
+        assertEq(maxSupply, 1_000_000_000 ether);
+        assertEq(name, "Veera Token");
+        assertEq(symbol, "VEERA");
+    }
+
+    function test_BSCMainnetConfig() public {
+        vm.chainId(56);
+        HelperConfig config = new HelperConfig();
+        (address initialAdmin, uint256 initialSupply, uint256 maxSupply, string memory name, string memory symbol) =
+            config.activeNetworkConfig();
+
+        assertEq(initialAdmin, 0xd2b8875b840D3BD574E1e6b440888e110632A0FD);
+        assertEq(initialSupply, 0 ether);
+        assertEq(maxSupply, 1_000_000_000 ether);
+        assertEq(name, "Veera Token");
+        assertEq(symbol, "VEERA");
+    }
+
+    function test_BSCTestnetConfig() public {
+        vm.chainId(97);
+        HelperConfig config = new HelperConfig();
+        (address initialAdmin, uint256 initialSupply, uint256 maxSupply, string memory name, string memory symbol) =
+            config.activeNetworkConfig();
+
+        assertEq(initialAdmin, 0x9FF0FB8e246ac58b17Acf9b7D43B76E2D2e6Bf03);
+        assertEq(initialSupply, 0 ether);
+        assertEq(maxSupply, 1_000_000_000 ether);
+        assertEq(name, "Veera Token");
+        assertEq(symbol, "VEERA");
+    }
+
+    function test_LocalConfigFallback() public {
+        vm.chainId(LOCAL_CHAINID);
+        HelperConfig config = new HelperConfig();
+        (address initialAdmin, uint256 initialSupply, uint256 maxSupply, string memory name, string memory symbol) =
+            config.activeNetworkConfig();
+
+        assertEq(initialAdmin, 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        assertEq(initialSupply, 1_000_000_000 ether);
+        assertEq(maxSupply, 1_000_000_000 ether);
+        assertEq(name, "Veera Token");
+        assertEq(symbol, "VEERA");
+    }
+}
